2355a35a15eac33ef7b756b0ff27090da351606f
Tamura Yukichi <tamura.yukichi@gmail.com>
2018-11-27 20:29:31 +0900

    [from now] 2018/11/27 20:29:31
     main.go | 27 +++++++++++++++++++++++++--
     1 file changed, 25 insertions(+), 2 deletions(-)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -19,12 +20,34 @@ func main() {
 		panic(err)
 	}
 
+	// list all tables
 	res, _ := db.Query("SHOW TABLES")
-
 	var table string
-
 	for res.Next() {
 		res.Scan(&table)
 		fmt.Println(table)
+
+		// each table
+		rows, err := db.Query(
+			"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?",
+			"sakila", table)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		var columnName, dataType string
+		for rows.Next() {
+			if err = rows.Scan(&columnName, &dataType); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if strings.Contains(dataType, "char") || strings.Contains(dataType, "text") {
+				// update string field with the value masked
+				_, _ = db.Exec(fmt.Sprintf("UPDATE %s SET %s = CONCAT(LEFT(%s, 1), '*****')", table, columnName, columnName))
+			}
+		}
+
+		//break
 	}
 }
